58c2f4806e076141e9af9ceefb400f40ee2e7d4e
dillahmwangi <mcharity241@gmail.com>
2022-10-25 02:17:25 -0600

    updates

---
+++ app/src/main/java/uk/cmd/kcauvotingapp/nominee_prof.java
@@ -0,0 +1,14 @@
+package uk.cmd.kcauvotingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class nominee_prof extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nominee_prof);
+    }
+}
