dbe833e84cb8c82e2c74ea3c3ddb7eac3cda43f3
dillahmwangi <mcharity241@gmail.com>
2022-11-03 13:33:20 -0600

    update

---
+++ app/src/main/java/uk/cmd/kcauvotingapp/ReyclerViewAdapter.java
@@ -0,0 +1,58 @@
+package uk.cmd.kcauvotingapp;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ReyclerViewAdapter extends RecyclerView.Adapter<ReyclerViewAdapter.MyViewHolder> {
+
+    Context context;
+    List<single_view> list;
+
+    public ReyclerViewAdapter(Context context, List<single_view> list) {
+        this.context = context;
+        this.list = list;
+    }
+
+    @NonNull
+    @Override
+    public ReyclerViewAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.single_view,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReyclerViewAdapter.MyViewHolder holder, int position) {
+        holder.profile_image.setImageResource(list.get(position).getprofile_image());
+        holder.txtUsername.setText(list.get(position).gettxtUsername());
+        holder.txtDescription.setText(list.get(position).gettxtDescription());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        CircleImageView profile_image;
+        TextView txtUsername, txtDescription;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            profile_image = itemView.findViewById(R.id.profile_image);
+            txtDescription = itemView.findViewById(R.id.txtDescription);
+            txtUsername = itemView.findViewById(R.id.txtUsername);
+        }
+    }
+}
--- app/src/main/java/uk/cmd/kcauvotingapp/single_view.java
+++ app/src/main/java/uk/cmd/kcauvotingapp/single_view.java
@@ -1,36 +1,38 @@
 package uk.cmd.kcauvotingapp;
 
 public class single_view {
-    private int imageProfile;
-    private String username,description;
+    private int profile_image;
+    private String txtUsername,txtDescription;
 
-    public single_view(int imageProfile, String username, String description) {
-        this.imageProfile = imageProfile;
-        this.username = username;
-        this.description = description;
+    public single_view(int imageProfile, String txtUsernameusername, String txtDescription) {
+        this.profile_image = profile_image;
+        this.txtUsername= txtUsername;
+        this.txtDescription = txtDescription;
     }
 
-    public int getImageProfile() {
-        return imageProfile;
+    public int getprofile_image() {
+        return profile_image;
     }
 
-    public void setImageProfile(int imageProfile) {
-        this.imageProfile = imageProfile;
+    public void setprofile_image(int profile_image) {
+        this.profile_image = profile_image;
     }
 
-    public String getUsername() {
-        return username;
+    public String gettxtUsername() {
+        return txtUsername;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void settxtUsername(String txtUsername) {
+        this.txtUsername = txtUsername;
     }
 
-    public String getDescription() {
-        return description;
+    public String gettxtDescription() {
+        return txtDescription;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void settxtDescription(String txtDescription) {
+        this.txtDescription = txtDescription;
     }
+
+
 }
