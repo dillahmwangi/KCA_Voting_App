43d77a061d1cac791f49b1862cd8dc84187becd9
dillahmwangi <mcharity241@gmail.com>
2022-11-06 16:19:22 -0600

    next commit

--- app/src/main/java/uk/cmd/kcauvotingapp/MainActivity.java
+++ app/src/main/java/uk/cmd/kcauvotingapp/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new Handler().postDelayed(new Runnable() {
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
 
 
             @Override
